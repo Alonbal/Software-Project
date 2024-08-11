726ba61259f5d7e60c20f6bafeab6ff1706fd217
Alonbal <alonbalas7@gmail.com>
2024-08-11 13:46:11 +0300

    Delete 213147572_322290750_project directory

--- 213147572_322290750_project/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("symnmfmodule", sources=['symnmfmodule.c', 'symnmf.c'])
-setup(name='symnmfmodule',
-     version='1.0',
-     description='Symnmf in C',
-     ext_modules=[module])
